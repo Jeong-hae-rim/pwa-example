c138b6f8f5e6d57c622099c17b6bea2625504140
jeong-hae-rim <usemono33@gmail.com>
2024-01-31 14:32:33 +0900

    feat: pwa complete

--- next.config.js
+++ next.config.js
@@ -1,40 +1,19 @@
-const path = require("path");
-const withPWAInit = require("next-pwa");
-
-/** @type {import('next-pwa').PWAConfig} */
-const withPWA = withPWAInit({
-  dest: "public",
-  // Solution: https://github.com/shadowwalker/next-pwa/issues/424#issuecomment-1399683017
-  buildExcludes: ["app-build-manifest.json"],
-});
-
-const generateAppDirEntry = (entry) => {
-  const packagePath = require.resolve('next-pwa');
-  const packageDirectory = path.dirname(packagePath);
-  const registerJs = path.join(packageDirectory, "register.js");
-
-  return entry().then((entries) => {
-    // Register SW on App directory, solution: https://github.com/shadowwalker/next-pwa/pull/427
-    if (entries["main-app"] && !entries["main-app"].includes(registerJs)) {
-      if (Array.isArray(entries["main-app"])) {
-        entries["main-app"].unshift(registerJs);
-      } else if (typeof entries["main-app"] === "string") {
-        entries["main-app"] = [registerJs, entries["main-app"]];
-      }
-    }
-    return entries;
-  });
-};
-
-/** @type {import('next').NextConfig} */
+// Configuration options for Next.js
 const nextConfig = {
-  reactStrictMode: true,
-  webpack: (config) => {
-    const entry = generateAppDirEntry(config.entry);
-    config.entry = () => entry;
-
-    return config;
+  reactStrictMode: true, // Enable React strict mode for improved error handling
+  swcMinify: true,      // Enable SWC minification for improved performance
+  compiler: {
+    removeConsole: process.env.NODE_ENV !== "development", // Remove console.log in production
   },
 };
 
+// Configuration object tells the next-pwa plugin 
+const withPWA = require("next-pwa")({
+  dest: "public", // Destination directory for the PWA files
+  disable: process.env.NODE_ENV === "development", // Disable PWA in development mode
+  register: true, // Register the PWA service worker
+  skipWaiting: true, // Skip waiting for service worker activation
+});
+
+// Export the combined configuration for Next.js with PWA support
 module.exports = withPWA(nextConfig);
